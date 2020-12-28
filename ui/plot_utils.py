c7e9962b50637ee146042fe85fc53e489117f078
kpekala <kgpekala@gmail.com>
2020-12-28 12:21:07 +0100

    added plot utils

---
+++ ui/plot_utils.py
@@ -0,0 +1,45 @@
+from random import randint
+
+def getLineSegments(plot):
+    segments = plot.get_added_elements().lines[0].lines
+    return segments
+
+
+def getPoints(plot):
+    seg = plot.get_added_elements().lines[0].lines
+    points = []
+
+    for l in seg:
+        points.append(l[0])
+        points.append(l[1])
+
+    return points
+
+
+def generateRandomLineSegments(n, min_x, max_x, min_y, max_y):
+    segments = []
+    s = set()
+
+    while n > 0:
+        x1 = randint(min_x, max_x)
+        x2 = randint(min_x, max_x)
+
+        y1 = randint(min_y, max_y)
+        y2 = randint(min_y, max_y)
+
+        if x1 == x2:
+            continue
+
+        if x1 in s or x2 in s:
+            continue
+
+        if x1 > x2:
+            segments.append(((x2, y2), (x1, y1)))
+        else:
+            segments.append(((x1, y1), (x2, y2)))
+
+        s.add(x1)
+        s.add(x2)
+        n -= 1
+
+    return segments
